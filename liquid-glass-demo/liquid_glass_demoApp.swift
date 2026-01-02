7b3f107c5c5d367c2ecd43892e59b7b6212534a6
Liam <khoasdyn@gmail.com>
2026-01-02 11:07:11 +0700

    Initial Commit

---
+++ liquid-glass-demo/liquid_glass_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  liquid_glass_demoApp.swift
+//  liquid-glass-demo
+//
+//  Created by khoasdyn on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct liquid_glass_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
